8ce780f40530a9dba8c679a844ad155e16366acb
lspedalieri <leopoldo.spedalieri@gmail.com>
2024-01-08 00:11:57 +0100

    Part 22 - Smoothing Camera script

--- player/PlayerCamera.gd
+++ player/PlayerCamera.gd
@@ -2,6 +2,20 @@ extends Camera2D
 
 @export var player: CharacterBody2D
 
+@export_category("Camera Smoothing")
+@export var smoothing_enabled: bool
+@export_range(1,10) var smoothing_distance: int = 8
+var weight: float
+
+func _ready():
+	weight = float(11 - smoothing_distance) /100
+
 func _physics_process(delta):
 	if player != null:
-		global_position = player.global_position
+		var camera_position: Vector2
+		if smoothing_enabled:
+			camera_position = lerp(global_position, player.global_position, weight)
+		else:
+			camera_position = player.global_position
+		print("Weight: ", weight, "  Camera position: ", camera_position, " - Camera Pixel Floor: ", camera_position.floor())
+		global_position = camera_position.floor()
